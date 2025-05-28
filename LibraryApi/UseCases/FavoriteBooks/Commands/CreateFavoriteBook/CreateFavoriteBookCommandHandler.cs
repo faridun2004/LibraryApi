9a1c277731d8acfd3957be2e50a32c8f8f2a1e01
juraev faridun <faridun11110001@gmail.com>
2025-05-28 18:09:50 +0500

    Добавлены проверки в CreateFavoriteBookCommandHandler
    В обработчике команды добавлены проверки на наличие запроса, существование пользователя и книги в базе данных.
    При отсутствии данных выбрасываются соответствующие исключения (`ArgumentNullException` и `InvalidOperationException`).
    Также реализована проверка на добавление книги в избранное, что улучшает обработку ошибок и надежность кода.

--- LibraryApi/UseCases/FavoriteBooks/Commands/CreateFavoriteBook/CreateFavoriteBookCommandHandler.cs
+++ LibraryApi/UseCases/FavoriteBooks/Commands/CreateFavoriteBook/CreateFavoriteBookCommandHandler.cs
@@ -1,6 +1,7 @@
 ﻿using LibraryApi.Data;
 using LibraryApi.Entities;
 using MediatR;
+using Microsoft.EntityFrameworkCore;
 
 namespace LibraryApi.UseCases.FavoriteBooks.Commands.CreateFavoriteBook
 {
@@ -15,6 +16,27 @@ namespace LibraryApi.UseCases.FavoriteBooks.Commands.CreateFavoriteBook
 
         public async Task<Guid> Handle(CreateFavoriteBookCommand request, CancellationToken cancellationToken)
         {
+            if (request is null)
+                throw new ArgumentNullException(nameof(request));
+            var data = await (
+                from user in _context.Users
+                where user.Id == request.UserId
+                from book in _context.Books
+                where book.Id == request.BookId
+                select new
+                {
+                    UserExists = true,
+                    BookExists = true,
+                    AlreadyFavorite = _context.FavoriteBooks
+                        .Any(fb => fb.UserId == request.UserId && fb.BookId == request.BookId)
+                }).FirstOrDefaultAsync(cancellationToken);
+
+            if (data == null)
+                throw new InvalidOperationException("Пользователь или книга не найдены.");
+
+            if (data.AlreadyFavorite)
+                throw new InvalidOperationException("Книга уже добавлена в избранное.");
+
             var favoriteBook = new FavoriteBook
             {
                 UserId = request.UserId,
